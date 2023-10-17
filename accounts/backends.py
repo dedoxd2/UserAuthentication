12672c3464c315345178c45f30eab6e424e211e2
dedoxd2 <dedo.xd33@yahoo.com>
2023-10-17 03:32:28 +0300

    add login with email or username

---
+++ accounts/backends.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.backends import ModelBackend
+from django.contrib.auth.models import User
+from django.db.models import Q
+
+
+class EmailBackend(ModelBackend):
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        """Checl Username & Password belongs to a valid user"""
+        try:
+            user = User.objects.get(
+                Q(username__iexact=username) | Q(email__iexact=username))
+        except User.DoesNotExist:
+            return None
+        except User.MultipleObjectsReturnes:
+            return User.objects.filter(email=username).order_by('id').first()
+        else:
+            if user.check_password(password) and self.user_can_authenticate(user):
+                return user
+
+    def get_user(self, user_id):
+        try:
+            user = User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
+
+        return user if self.user_can_authenticate(user) else None
